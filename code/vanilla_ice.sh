a5d37ead9e106546e15c15131e8d607c5b05b118
David Astling <dastling@gmail.com>
2016-03-09 14:11:11 -0700

    add shell script for running vanillaICE

---
+++ code/vanilla_ice.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#BSUB -J R[3001-3739]%10
+#BSUB -e logs/vanilla_ice_%J.log
+#BSUB -o logs/vanilla_ice_%J.out
+#BSUB -q normal
+#BSUB -n 12
+#BSUB -R "select[mem>40] rusage[mem=40] span[hosts=1]"
+#BSUB -P Tamim
+
+# 3739
+
+DATA=data/Spritz_release_genotype_files
+RESULTS=results/vanilla_ice
+SAMPLES=($DATA/*_FinalReport_*.csv)
+sample_file=${SAMPLES[$(($LSB_JOBINDEX - 1))]}
+
+Rscript code/vanilla_ice_single.R $sample_file $RESULTS
+
