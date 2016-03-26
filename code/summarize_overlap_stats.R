db475118f1fcfe00b619c2091b06475c95140c7c
David Astling <dastling@gmail.com>
2016-03-26 08:21:33 -0600

    add code for summarizing overlaps from three programs

---
+++ code/summarize_overlap_stats.R
@@ -0,0 +1,29 @@
+
+options(stringsAsFactors = FALSE)
+
+library(tidyr)
+library(dplyr)
+
+df <- read.delim("stats.txt", header = FALSE)
+colnames(df) <- c("file", "chr", "pos1", "pos2", "n", "stat")
+
+df <- mutate(df, file = gsub("results/vanilla_ice_qc/", "", file))
+df <- mutate(df, file = gsub("_overlap.txt", "", file))
+df <- mutate(df, file = gsub(".bed", "_bed", file))
+df <- filter(df, pos2 - pos1 > 0)
+
+df <- df %>% 
+    group_by(file) %>% 
+	summarise(
+		number.of.regions = length(pos1),
+		mean.log.length = mean(log(pos2 - pos1, 10)),
+		mean.length = mean(pos2 - pos1),
+		mean.probes = mean(n),
+		mean.score  = mean(stat, na.rm = TRUE)
+		)
+
+df <- separate(df, file, c("patient", "id", "alg.1", "alg.2"), sep = "[_\\.]")
+
+write.table(df, file = "full_comparison.txt", sep = "\t", quote = FALSE, row.names = FALSE)
+
+
