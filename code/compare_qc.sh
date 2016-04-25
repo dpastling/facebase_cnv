8098933579bf5d46aefb6632618fa93bba97b312
David Astling <dastling@gmail.com>
2016-04-25 06:03:51 -0600

    overhaul of compare script
    - use logR data
    - generate bed file for each algorithm
    - consistant columns for each

--- code/compare_qc.sh
+++ code/compare_qc.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+#BSUB -J compare
+#BSUB -o logs/compare_qc_%J.out
+#BSUB -e logs/compare_qc_%J.err
 
 SAMPLES=(
 "18508.1064691934"
@@ -18,9 +21,10 @@ SAMPLES=(
 )
 
 valid_folder=data/validation
-penn_folder=results/penncnv
-dna_folder=results/dnacopy
-vice_folder=results/vanilla_ice
+data_folder=results/logR
+penn_folder=$data_folder/penncnv
+dna_folder=$data_folder/dnacopy
+vice_folder=$data_folder/vice
 results_folder=results/vanilla_ice_qc
 
 for sample in ${SAMPLES[@]}
@@ -29,9 +33,9 @@ do
     orig_id=`echo $sample | sed 's/\./\@/'`
     
     validation_bed=$valid_folder/estd20_NA${id}_5plus_probes.bed
-    vice_file=${vice_folder}/VICE*$orig_id*
-    penn_bed=${penn_folder}/sample*${orig_id}.tabcnv
-    dna_file=${dna_folder}/segment_pval*_${sample}.txt
+    vice_file=${vice_folder}/*$orig_id.txt
+    penn_file=${penn_folder}/*${orig_id}.txt
+    dna_file=${dna_folder}/*${orig_id}.txt
     
     # for DNAcopy
     #results/dnacopy/segment_pvalue_X10.1064639586.txt | cut -f 2- | tail -n +2
@@ -42,8 +46,15 @@ do
     
     vice_bed=$results_folder/${sample}_vice.bed
     dna_bed=$results_folder/${sample}_dna.bed
+    penn_bed=$results_folder/${sample}_penn.bed
     tail -n +2 $vice_file | sed 's/chr//' | awk '$7 < 3 || $7 > 3' | grep -v "^[XYM]" > $vice_bed
     tail -n +2 $dna_file | cut -f 2- | grep -v "^[XYM]" > $dna_bed
+    tail -n +2 $penn_file > $penn_bed
+
+    # filter copy number 2 regions from the DNAcopy data
+    awk '$11 < -0.1 || $11 > 0.1' $dna_bed > temp.bed
+    mv temp.bed $dna_bed
+
     prefix=$results_folder/${sample}
     bedtools intersect -u -a $validation_bed -b $vice_bed > ${prefix}_valid_vice_overlap.txt
     bedtools intersect -u -a $validation_bed -b $penn_bed > ${prefix}_valid_penn_overlap.txt
@@ -58,30 +69,48 @@ do
     bedtools intersect -u -a $dna_bed -b $vice_bed > ${prefix}_dna_vice_overlap.txt
     bedtools intersect -u -a $dna_bed -b $validation_bed > ${prefix}_dna_valid_overlap.txt
 
-    cp $validation_bed ${prefix}_valid_bed_overlap.txt
-    cp $penn_bed ${prefix}_penn_bed_overlap.txt
+    cp $validation_bed ${prefix}_valid.bed
     
+done
+
+## Get inverse overlaps, regions not overlapping with the other file
+for bed in $results_folder/*.bed
+do
+    prefix=`echo $bed | sed 's/.bed//'`
+    for x in ${prefix}*_overlap.txt
+    do
+        output=`echo $x | sed 's/_overlap/_inverse/'`
+        bedtools intersect -v -a $bed -b $x > $output
+    done
+done
+
+
+# print results to STDOUT
+for sample in ${SAMPLES[@]}
+do
+    prefix=$results_folder/${sample}
+
     for x in ${prefix}_valid*
     do
-        awk -F "\t" 'OFS="\t" {print $1, $2, $3, $6, "NA"}' $x | sed "s|^|$x\t|"
+        awk -F "\t" 'OFS="\t" {print $1, $2, $3, $6, "NA", "NA", "NA", "NA"}' $x | sed "s|^|$x\t|"
         #echo $x `awk -F "\t" '{l += $3 - $2; n += $6} END { print NR, l/NR, n/NR, "NA"}' $x`
     done
     
     for x in ${prefix}_vice*
     do
-        awk -F "\t" 'OFS="\t" {print $1, $2, $3, $6, $8}' $x | sed "s|^|$x\t|"
+        awk -F "\t" 'OFS="\t" {print $1, $2, $3, $6, $8, $10, $11, $12}' $x | sed "s|^|$x\t|"
         #echo $x `awk -F "\t" '{l += $3 - $2; n += $6; s += $8} END { print NR, l/NR, n/NR, s/NR}' $x`
     done
     
     for x in ${prefix}_penn*
     do
-        awk -F "\t" 'OFS="\t" {print $1, $2, $3, $9, $8}' $x | sed "s|^|$x\t|"
+        awk -F "\t" 'OFS="\t" {print $1, $2, $3, $9, $8, $10, $11, $12}' $x | sed "s|^|$x\t|"
         #echo $x `awk -F "\t" '{l += $3 - $2; n += $9; s += $8} END { print NR, l/NR, n/NR, s/NR}' $x`
     done
     
     for x in ${prefix}_dna*
     do
-        awk -F "\t" 'OFS="\t" {print $1, $2, $3, $4, $7}' $x | sed "s|^|$x\t|"
+        awk -F "\t" 'OFS="\t" {print $1, $2, $3, $4, $7, $10, $11, $12}' $x | sed "s|^|$x\t|"
         #echo $x `awk '{l += $3 - $2; n += $4; s += $7} END { print NR, l/NR, n/NR, s/NR}' $x`
     done
 done
