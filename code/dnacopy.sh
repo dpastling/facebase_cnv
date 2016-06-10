1ce08bc7df6f9e0d31a49c0f103637954acd60f0
David Astling <dastling@gmail.com>
2016-06-10 13:20:16 -0600

    change dnacopy alpha paramater to 0.1
    - this new parameter gives shorter segments that are more in tune with
      PennCNV
    - also delete other code that saves multiple result files and plots.
      They were helpful in the begining, but are no longer necessary.

--- code/dnacopy.sh
+++ code/dnacopy.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-#BSUB -J dnacopy[701-1000]
+#BSUB -J dnacopy[2001-3000]%5
 #BSUB -e logs/dnacopy_%J.log
 #BSUB -o logs/dnacopy_%J.out
-#BSUB -q test
+#BSUB -q normal
 #BSUB -R "select[mem>10] rusage[mem=10]"
 #BSUB -P Tamim
 
+#3739
+
 DATA=data/penncnv
-RESULTS=results/dnacopy
+RESULTS=results/dnacopy_alpha0_1
 SAMPLES=($DATA/FinalReport_*)
 sample_file=${SAMPLES[$(($LSB_JOBINDEX - 1))]}
 
--- code/dnacopy_bySample.R
+++ code/dnacopy_bySample.R
@@ -38,46 +38,49 @@ CNA.object <- CNA(
 
 ## Clean Data
 smoothed.CNA.object <- smooth.CNA(CNA.object)
-segment.smoothed.CNA.object <- segment(smoothed.CNA.object, verbose = 1)
+segments <- segment(smoothed.CNA.object, min.width = 5, alpha = 0.1, verbose = 1)
 
+#segment.smoothed.CNA.object <- segment(smoothed.CNA.object, verbose = 1)
+#
+#
+#
+### Visualize Data
+#
+#custom_png <- function(file)
+#{
+#	png(file, width = 1800, height = 1500, res = 300, bg = "white")
+#}
+#
+#### Plot all chromosomes on one scatter plot
+#custom_png(file = paste0(plots.folder, "/genome_wide_", sample.name, ".png"))
+#plot(segment.smoothed.CNA.object, plot.type = "w")
+#dev.off()
+#
+#### Individual faceted plots for each chromosome 
+#custom_png(file = paste0(plots.folder, "/by_chr_", sample.name, ".png"))
+#plot(segment.smoothed.CNA.object, plot.type = "s")
+#dev.off()
+#
+#### Don't understand the point of this plot, maybe means for each chromosome?
+#custom_png(file = paste0(plots.folder, "/segment_means_", sample.name, ".png"))
+#plot(segment.smoothed.CNA.object, plot.type = "p")
+#dev.off()
+#
+#
+## Remove segments that are not at least 3 SDs apart
+#sdundo.CNA.object <- segment(
+#	smoothed.CNA.object,
+#	undo.splits = "sdundo",
+#	undo.SD = 3,
+#	verbose = 1
+#)
 
+#segment.smoothed.p <- segments.p(segment.smoothed.CNA.object)
+#sdundo.p <- segments.p(sdundo.CNA.object)
+#sdundo.p <- filter(sdundo.p, num.mark >= 5)
 
-## Visualize Data
-
-custom_png <- function(file)
-{
-	png(file, width = 1800, height = 1500, res = 300, bg = "white")
-}
-
-### Plot all chromosomes on one scatter plot
-custom_png(file = paste0(plots.folder, "/genome_wide_", sample.name, ".png"))
-plot(segment.smoothed.CNA.object, plot.type = "w")
-dev.off()
-
-### Individual faceted plots for each chromosome 
-custom_png(file = paste0(plots.folder, "/by_chr_", sample.name, ".png"))
-plot(segment.smoothed.CNA.object, plot.type = "s")
-dev.off()
-
-### Don't understand the point of this plot, maybe means for each chromosome?
-custom_png(file = paste0(plots.folder, "/segment_means_", sample.name, ".png"))
-plot(segment.smoothed.CNA.object, plot.type = "p")
-dev.off()
-
-
-# Remove segments that are not at least 3 SDs apart
-sdundo.CNA.object <- segment(
-	smoothed.CNA.object,
-	undo.splits = "sdundo",
-	undo.SD = 3,
-	verbose = 1
-)
-
-segment.smoothed.p <- segments.p(segment.smoothed.CNA.object)
-sdundo.p <- segments.p(sdundo.CNA.object)
-sdundo.p <- filter(sdundo.p, num.mark >= 5)
-
-write.table(segment.smoothed.CNA.object$output, file = paste0(output.folder, "/segment_smoothed_", sample.name, ".txt"), sep = "\t", quote = FALSE, row.names = FALSE) 
-write.table(sdundo.CNA.object$output, file = paste0(output.folder, "/sdundo_", sample.name, ".txt"), sep = "\t", quote = FALSE, row.names = FALSE)
-write.table(segment.smoothed.p, file = paste0(output.folder, "/segment_pvalue_", sample.name, ".txt"), sep = "\t", quote = FALSE, row.names = FALSE)
-write.table(sdundo.p, file = paste0(output.folder, "/sdundo_pvalue_", sample.name, ".txt"), sep = "\t", quote = FALSE, row.names = FALSE)
+write.table(segments$output, file = paste0(output.folder, "/segment_alpha0_1_", sample.name, ".txt"), sep = "\t", quote = FALSE, row.names = FALSE) 
+#write.table(segment.smoothed.CNA.object$output, file = paste0(output.folder, "/segment_smoothed_", sample.name, ".txt"), sep = "\t", quote = FALSE, row.names = FALSE) 
+#write.table(sdundo.CNA.object$output, file = paste0(output.folder, "/sdundo_", sample.name, ".txt"), sep = "\t", quote = FALSE, row.names = FALSE)
+#write.table(segment.smoothed.p, file = paste0(output.folder, "/segment_pvalue_", sample.name, ".txt"), sep = "\t", quote = FALSE, row.names = FALSE)
+#write.table(sdundo.p, file = paste0(output.folder, "/sdundo_pvalue_", sample.name, ".txt"), sep = "\t", quote = FALSE, row.names = FALSE)
