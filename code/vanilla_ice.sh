0f209b0bcdc1b544b0ba335c32dd3be2ecc5af01
David Astling <david.astling@ucdenver.edu>
2016-12-29 17:13:04 -0700

    brute force specific file with vanilla ice
    - use older version of R
    - this file would not process with regular code

--- code/vanilla_ice.sh
+++ code/vanilla_ice.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#BSUB -J R[3001-3739]%3
-#BSUB -e logs/vanilla_ice_%J.log
-#BSUB -o logs/vanilla_ice_%J.out
+#BSUB -J R
+#BSUB -e logs/vanilla_ice_%J_%I.log
+#BSUB -o logs/vanilla_ice_%J_%I.out
 #BSUB -q normal
 #BSUB -n 12
 #BSUB -R "select[mem>40] rusage[mem=40] span[hosts=1]"
@@ -10,10 +10,16 @@
 # 3739
 
 DATA=data/Spritz_release_genotype_files
-#RESULTS=results/vanilla_ice
-RESULTS=data/corrected_vals
-SAMPLES=($DATA/*_FinalReport_*.csv)
-sample_file=${SAMPLES[$(($LSB_JOBINDEX - 1))]}
+RESULTS=results/vanilla_ice
+#RESULTS=data/corrected_vals
+#SAMPLES=($DATA/*_FinalReport_*.csv)
+#sample_file=${SAMPLES[$(($LSB_JOBINDEX - 1))]}
 
-Rscript code/vanilla_ice_single.R $sample_file $RESULTS
+#SAMPLES=(`cat vice_missing.txt`)
+#sample_file=${SAMPLES[$(($LSB_JOBINDEX - 1))]}
+#sample_file=$DATA/*_FinalReport_$sample_file.csv
+
+sample_file=$DATA/*FinalReport_1141@0123840970.csv
+
+$HOME/R-3.2.3/bin/Rscript code/vanilla_ice_single.R $sample_file $RESULTS
 
