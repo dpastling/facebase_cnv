786044c2bed87f42b6d9cd13df81afb11f95634a
David Astling <david.astling@ucdenver.edu>
2016-12-29 17:11:36 -0700

    fix conflicts with dplyr and bioconductor
    - change order in which libraries are loaded
    - for dplyr functions specify with dplyr::
    - remove filter of confidence of 0.95

--- code/vanilla_ice_single.R
+++ code/vanilla_ice_single.R
@@ -1,12 +1,10 @@
-#!/usr/bin/env Rscript
 
 options(stringsAsFactors = FALSE)
 
-library(plyr)
 library(dplyr)
+library(BSgenome.Hsapiens.UCSC.hg19)
 library(ArrayTV)
 library(VanillaICE)
-library(BSgenome.Hsapiens.UCSC.hg19)
 library(doSNOW)
 #library(doParallel)
 
@@ -14,11 +12,15 @@ args <- commandArgs(trailingOnly = TRUE)
 
 stopifnot(length(args) == 2)
 
+# for testing
+# file.name <- "data/Spritz_release_genotype_files/Spritz_Omni2-5PlusExome_release_FinalReport_1110@0123841633.csv"
+# output.folder <- "results/vanilla_ice"
+
 file.name     <- args[1]
 output.folder <- args[2]
 sample.name   <- gsub("^.+?FinalReport_(.+?).csv", "\\1", file.name)
 
-cl <- makeCluster(12, type = "SOCK")
+cl <- makeCluster(6, type = "SOCK")
 registerDoSNOW(cl)
 
 
@@ -29,15 +31,15 @@ features <- read.csv(features.data, header = TRUE, skip = 7)
 # be labeled like [A/T], while control probes are called something like "DNP (Bgnd)"
 # we will use the presence of an opening bracket as evidence for a SNP
 # Anyway, it turns out that all mapped probes have an IntensityOnly value of 0
-features <- mutate(features, IntensityOnly = as.integer(! grepl("\\[", SNP)))
-features <- select(features, Name, Chr, MapInfo, IntensityOnly)
+features <- dplyr::mutate(features, IntensityOnly = as.integer(! grepl("\\[", SNP)))
+features <- dplyr::select(features, Name, Chr, MapInfo, IntensityOnly)
 
 X <- read.csv(file.name, header = TRUE, skip = 10)
 X <- inner_join(X, features, by = c("SNP.Name" = "Name"))
 X <- X %>% 
-    filter(! Chr %in% c("0", "MT", "XY")) %>% 
-    filter(! is.na(Log.R.Ratio)) %>%
-    arrange(as.numeric(Chr), MapInfo)
+    dplyr::filter(! Chr %in% c("0", "MT", "XY")) %>% 
+    dplyr::filter(! is.na(Log.R.Ratio)) %>%
+    dplyr::arrange(as.numeric(Chr), MapInfo)
 X <- as.data.frame(X)
 
 max.window <- c(100, 10e3, 1e6)
@@ -46,7 +48,7 @@ increms <- c(20, 2000, 200e3)
 # For some reason, ArrayTV requires the data to be in a matrix. 
 # the object needs to be a one rolumn matrix for it to work.# alternativly you can pass along objects of type ‘BeadStudioSet’, ‘BafLrrSet’, or ‘BafLrrSetList’
 # but is much easier just to select the single column
-temp   <- select(X, Log.R.Ratio, MapInfo)
+temp   <- dplyr::select(X, Log.R.Ratio, MapInfo)
 temp   <- as.matrix(temp)
 tvList <- gcCorrect(object = temp[, "Log.R.Ratio", drop = FALSE],
 	chr = paste0("chr", X$Chr),
@@ -56,12 +58,10 @@ tvList <- gcCorrect(object = temp[, "Log.R.Ratio", drop = FALSE],
 	build = 'hg19'
 	)
 
-X <- mutate(X, corrected.vals = tvList$correctedVals)
+X <- dplyr::mutate(X, corrected.vals = tvList$correctedVals)
 
 # save corrected values
 write.table(X, file = paste0(output.folder, "/corrected_vals_", sample.name, ".txt"), sep = "\t", quote = FALSE, row.names = FALSE)
-
-q(save = "no")
 
 # plot before and after Log.R.Ratios
 
@@ -94,11 +94,10 @@ snp_expt <- SnpArrayExperiment(
 param    <- EmissionParam()
 fit     <- hmm2(snp_expt, param)
 
-stopCluster(cl)
-
-filter_param <- FilterParam(numberFeatures = 5, probability = 0.95)
+filter_param <- FilterParam(numberFeatures = 5)
 result <- cnvSegs(fit, filter_param)
 result <- as.data.frame(result)
 write.table(result, file = paste0(output.folder, "/VICE_", sample.name, ".txt"), sep = "\t", quote = FALSE, row.names = FALSE)
 
+stopCluster(cl)
 
