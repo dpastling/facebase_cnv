026f3f8090db1ca1966600bdc02c4a1b6c81dc5a
David Astling <david.astling@ucdenver.edu>
2017-06-28 10:23:27 -0600

    minor bug fixes to penncnv.sh
    - fix results folder to PENNCNV_RESULTS instead of RESULTS
    - add a check if LSF is present. It's possible someone will try to run
      the script without LSF. Also  helpful for testing on a local machine
    - update hash bang

--- code/penncnv.sh
+++ code/penncnv.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #BSUB -J penncnv[1-3]
 #BSUB -e logs/penncnv_%J.log
 #BSUB -o logs/penncnv_%J.out
@@ -6,9 +6,20 @@
 #BSUB -R "select[mem>10] rusage[mem=10]"
 #BSUB -P Facebase
 
-#3739
+# Note to self:
+#   3 example samples
+#   3739 Bantu samples
+#   3330 European-American samples
 
 source code/config_example.sh
+
+# If not running on a server with LSF, allow the script to run
+# but process only the first sample. Helpful for testing purposes
+if [ -z $LSB_JOBINDEX ]; then
+	echo "Warning: LSF queing system not found!"
+	echo "         Processing only the first sample in the list"
+	LSB_JOBINDEX=1
+fi
 
 sample_file=${SAMPLES[$(($LSB_JOBINDEX - 1))]}
 sample=`echo $sample_file | perl -pe 's/^.+?\/[^\/]+$/$1/'`
@@ -22,8 +33,8 @@ detect_cnv.pl \
 --hmm $hmm_file \
 --pfb $pfb_file \
 --gcmodel $gc_model \
---log $RESULTS/sample_${sample}.log \
---out $RESULTS/sample_${sample}.rawcnv \
+--log $PENNCNV_RESULTS/sample_${sample}.log \
+--out $PENNCNV_RESULTS/sample_${sample}.rawcnv \
 $sample_file
 
 convert_cnv.pl --intype penncnv --outtype tab --output $RESULTS/sample_${sample}.tabcnv $RESULTS/sample_${sample}.rawcnv
