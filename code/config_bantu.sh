bf19c2a5dc5d544c693e91f982b575460dceeaca
David Astling <david.astling@ucdenver.edu>
2017-06-28 10:00:14 -0600

    create a config script for the Bantu samples
    - set paths to results folders
    - create empty folders if missing
    - create variables for needed marker info files

---
+++ code/config_bantu.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+DATA=data/penncnv
+PENNCNV_RESULTS=results/penncnv
+DNACOPY_RESULTS=results/dnacopy
+VICE_RESULTS=results/vanilla_ice
+SAMPLES=($DATA/FinalReport_*)
+
+hmm_file=data/IlluminaHumanCoreExome_v12-A.hmm
+pfb_file=data/Human_Omni25exome.pfb
+gc_model=data/Human_Omni25exome_GCModel.txt
+
+
