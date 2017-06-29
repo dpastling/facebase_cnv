3fdf2e8449761553b45b3b4030ea3e7f70fab09e
David Astling <david.astling@ucdenver.edu>
2017-06-29 13:40:18 -0600

    remove unused code from dnacopy R script

--- code/dnacopy_bySample.R
+++ code/dnacopy_bySample.R
@@ -5,31 +5,21 @@ library(DNAcopy)
 library(dplyr)
 
 args <- commandArgs(trailingOnly = TRUE)
-
 stopifnot(length(args) == 2)
-
 file.name     <- args[1]
 output.folder <- args[2]
 sample.name   <- gsub("^.+?_([^_]+).txt", "\\1", file.name)
-#output.folder <- "results/dnacopy"
-#plots.folder  <- "plots/dnacopy"
 
-#X <- read.delim(file.name, check.names = FALSE)
 X <- read.delim(file.name)
-
-#array.info <- read.delim("data/Marazitafacialvariation_11779814_A.pfb")
-#array.info = array.info[, -4]
-
-#X <- left_join(X, array.info, by = c('Name' = 'SNP'))
-#X <- left_join(X, array.info, by = c('SNP.Name' = 'Name'))
 
 # remove probes labeled chr 0, these might be for quality control
 X <- X[X$Chr != 0, ]
 
-#lrr.column <- grep("Log.R.Ratio", colnames(X))
-#sample.name <- gsub(".Log.R.Ratio", "", colnames(X)[lrr.column])
-
+# Assume we are using the GC corrected values generated from ArrayTV
 lrr.column <- grep("corrected.vals", colnames(X))
+# TODO: check if "corrected.vals" column was found. If not, try to guess at LogR column
+#       stopifnot the required columns not present
+
 sample.name <- X[1, "Sample.Name"]
 
 CNA.object <- CNA( 
@@ -44,7 +34,5 @@ CNA.object <- CNA(
 smoothed.CNA.object <- smooth.CNA(CNA.object)
 segments <- segment(smoothed.CNA.object, min.width = 5, alpha = 0.1, verbose = 1)
 
-
 write.table(segments$output, file = paste0(output.folder, "/segment_alpha0_1_", sample.name, ".txt"), sep = "\t", quote = FALSE, row.names = FALSE) 
-
 
