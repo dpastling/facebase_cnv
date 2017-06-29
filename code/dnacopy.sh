75d2e7a92b78e0cbdcfedb9634856b97333e0ebc
David Astling <david.astling@ucdenver.edu>
2017-06-29 13:39:44 -0600

    move hard coded paths from dnacopy to config

--- code/dnacopy.sh
+++ code/dnacopy.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-#BSUB -J dnacopy[2001-3000]%5
+#BSUB -J dnacopy[1-3]%5
 #BSUB -e logs/dnacopy_%J.log
 #BSUB -o logs/dnacopy_%J.out
 #BSUB -q normal
 #BSUB -R "select[mem>10] rusage[mem=10]"
-#BSUB -P Tamim
+#BSUB -P Facebase
 
-#3739
+# TODO: need to check if input to DNAcopy is ok
+# DATA=data/penncnv
 
-DATA=data/penncnv
-RESULTS=results/dnacopy_alpha0_1
-SAMPLES=($DATA/FinalReport_*)
+source code/config_example.sh
+
 sample_file=${SAMPLES[$(($LSB_JOBINDEX - 1))]}
 
-Rscript code/dnacopy_bySample.R $sample_file $RESULTS
+Rscript code/dnacopy_bySample.R $sample_file $DNACOPY_RESULTS
 
